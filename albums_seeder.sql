0e4073d42c6218bd419f34d53ca0b13b82f0524c
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-08-25 15:22:32 -0500

    got wikipedia exercise done

---
+++ albums_seeder.sql
@@ -0,0 +1,11 @@
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop, post-disco, funk, rock', 50.2),
+       ('AC/DC', 'Back in Black', 1980, 'Hard rock', 30.1),
+       ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 'Country rock, soft rock, folk rock', 41.2),
+       ('Eagles', 'Hotel California', 1976, 'Soft rock', 31.8),
+       ('Led Zeppelin', 'Led Zeppelin IV', 1971, 'Hard rock', 30.4)
+
+
+
+
+
