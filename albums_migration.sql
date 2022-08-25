9104b6607e5216e945a2dfb04eb8bd960e387555
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-08-25 13:33:11 -0500

    sql table complete

---
+++ albums_migration.sql
@@ -0,0 +1,18 @@
+CREATE TABLE quotes (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        author_first_name VARCHAR(50) DEFAULT 'NONE',
+                        author_last_name  VARCHAR(100) NOT NULL,
+                        content TEXT NOT NULL,
+                        PRIMARY KEY (id)
+);
+
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist VARCHAR(50) DEFAULT 'NONE',
+                        name  VARCHAR(100) NOT NULL,
+                        release_date INT,
+--                         sales FLOAT UNSIGNED,
+                        sales DECIMAL(4,2) UNSIGNED,
+                        genre VARCHAR(50)
+                        PRIMARY KEY (id)
+);
